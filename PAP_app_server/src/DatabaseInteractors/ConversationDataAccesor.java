19232011d3c03116233bfbeb0cdd0f35306c5ea4
mchomans <01168876@pw.edu.pl>
2023-03-18 16:15:27 +0100

    refactor the code

--- PAP_app_server/src/DatabaseInteractors/ConversationDataAccesor.java
+++ PAP_app_server/src/DatabaseInteractors/ConversationDataAccesor.java
@@ -5,18 +5,18 @@ import java.util.Hashtable;
 
 public class ConversationDataAccesor implements DataAccesorInterface {
     public static Hashtable<String, String> get_data(int id) {
-        Hashtable<String, String> usr_data = new Hashtable<String, String>();
-        return usr_data;
+        Hashtable<String, String> data = new Hashtable<String, String>();
+        return data;
     }
 
     public static Hashtable<String, String> get_data(String column_name, String column_value) {
-        Hashtable<String, String> usr_data = new Hashtable<String, String>();
-        return usr_data;
+        Hashtable<String, String> data = new Hashtable<String, String>();
+        return data;
     }
 
     public static Hashtable<String, String> get_data(String column_name, int column_value) {
-        Hashtable<String, String> usr_data = new Hashtable<String, String>();
-        return usr_data;
+        Hashtable<String, String> data = new Hashtable<String, String>();
+        return data;
     }
 
     public static ArrayList<Integer> get_users_in_conversation(String email) {
