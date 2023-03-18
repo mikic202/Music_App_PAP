80677370a2ef09f1239111fad006490effcb6fec
mchomans <01168876@pw.edu.pl>
2023-03-18 14:21:14 +0100

    change all of the methods to static

--- PAP_app_server/src/DatabaseInteractors/DataAccesorInterface.java
+++ PAP_app_server/src/DatabaseInteractors/DataAccesorInterface.java
@@ -3,10 +3,19 @@ package DatabaseInteractors;
 import java.util.Hashtable;
 
 public interface DataAccesorInterface {
-    Hashtable<String, String> get_data(int id);
+    static Hashtable<String, String> get_data(int id) {
+        Hashtable<String, String> data = new Hashtable<String, String>();
+        return data;
+    }
 
-    Hashtable<String, String> get_data(String column_name, String column_value);
+    static Hashtable<String, String> get_data(String column_name, String column_value) {
+        Hashtable<String, String> data = new Hashtable<String, String>();
+        return data;
+    }
 
-    Hashtable<String, String> get_data(String column_name, int column_value);
+    static Hashtable<String, String> get_data(String column_name, int column_value) {
+        Hashtable<String, String> data = new Hashtable<String, String>();
+        return data;
+    }
 
 }
