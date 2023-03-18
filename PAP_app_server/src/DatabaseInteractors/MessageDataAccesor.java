501c65118c039a073bd0e85fc2cf79a26e203c94
mchomans <01168876@pw.edu.pl>
2023-03-18 16:14:18 +0100

    refactor method names and function

--- PAP_app_server/src/DatabaseInteractors/MessageDataAccesor.java
+++ PAP_app_server/src/DatabaseInteractors/MessageDataAccesor.java
@@ -4,22 +4,22 @@ import java.util.Hashtable;
 
 public class MessageDataAccesor implements DataAccesorInterface {
     public static Hashtable<String, String> get_data(int id) {
-        Hashtable<String, String> usr_data = new Hashtable<String, String>();
-        return usr_data;
+        Hashtable<String, String> data = new Hashtable<String, String>();
+        return data;
     }
 
     public static Hashtable<String, String> get_data(String column_name, String column_value) {
-        Hashtable<String, String> usr_data = new Hashtable<String, String>();
-        return usr_data;
+        Hashtable<String, String> data = new Hashtable<String, String>();
+        return data;
     }
 
     public static Hashtable<String, String> get_data(String column_name, int column_value) {
-        Hashtable<String, String> usr_data = new Hashtable<String, String>();
-        return usr_data;
+        Hashtable<String, String> data = new Hashtable<String, String>();
+        return data;
     }
 
-    public static Hashtable<String, String> get_data_with_email(String email) {
-        Hashtable<String, String> usr_data = new Hashtable<String, String>();
-        return usr_data;
+    public static String get_message_text(int message_id) {
+        String text = new String();
+        return text;
     }
 }
