97854e036b70715b1f551607a7e3604f4a8e7c2a
mchomans <01168876@pw.edu.pl>
2023-03-18 11:07:14 +0100

    add simple database interactor

--- PAP_app_server/src/DatabaseInteractors/UserDataAccesor.java
+++ PAP_app_server/src/DatabaseInteractors/UserDataAccesor.java
@@ -1,5 +1,12 @@
 package DatabaseInteractors;
 
+import java.util.Hashtable;
+
 public interface UserDataAccesor {
+    Hashtable<String, String> get_user_data(int user_id);
+
+    Hashtable<String, String> get_user_data(String username);
+
+    Hashtable<String, String> get_user_data_using_email(String email);
 
 }
