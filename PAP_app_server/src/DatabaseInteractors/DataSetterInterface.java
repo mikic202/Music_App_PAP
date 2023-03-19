2b534c46a1e9c22a46af407f2ef1cc0facb86885
mchomans <01168876@pw.edu.pl>
2023-03-19 17:00:36 +0100

    changed all methods to static

--- PAP_app_server/src/DatabaseInteractors/DataSetterInterface.java
+++ PAP_app_server/src/DatabaseInteractors/DataSetterInterface.java
@@ -3,7 +3,9 @@ package DatabaseInteractors;
 import java.util.Hashtable;
 
 public interface DataSetterInterface {
-    void set_data(int id, Hashtable<String, String> data);
+    static void set_data(int id, Hashtable<String, String> data) {
+    }
 
-    void add_data(Hashtable<String, String> data);
+    static void add_data(Hashtable<String, String> data) {
+    }
 }
