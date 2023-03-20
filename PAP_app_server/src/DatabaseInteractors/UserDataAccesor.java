607c50ba63dd2babbb927616845a80677dfb9ce2
mchomans <01168876@pw.edu.pl>
2023-03-20 15:52:08 +0100

    implement all mathods and make them work

--- PAP_app_server/src/DatabaseInteractors/UserDataAccesor.java
+++ PAP_app_server/src/DatabaseInteractors/UserDataAccesor.java
@@ -10,15 +10,69 @@ import java.sql.ResultSet;
 
 public class UserDataAccesor implements DataAccesorInterface {
 
-    final static String TABLENAME = "";
+    final static String TABLENAME = DatabseInformation.USER_TABLE.value();
 
     public static Hashtable<String, String> get_data(int user_id) {
 
-        Hashtable<String, String> user_data = new Hashtable<String, String>();
-
         String query = String.format("Select * from %s where user_id='%d'", TABLENAME, user_id);
 
-        ResultSet result = get_querryResult(query);
+        return get_querry_result(query);
+    }
+
+    public static Hashtable<String, String> get_data(String column_name, String column_value) {
+
+        String query = String.format("Select * from %s where %s='%s'", TABLENAME, column_name, column_value);
+
+        return get_querry_result(query);
+    }
+
+    public static Hashtable<String, String> get_data(String column_name, int column_value) {
+
+        String query = String.format("Select * from %s where %s='%d'", TABLENAME, column_name, column_value);
+
+        return get_querry_result(query);
+    }
+
+    public static Hashtable<String, String> get_data_with_email(String email) {
+        String query = String.format("Select * from %s where email='%s'", TABLENAME, email);
+        return get_querry_result(query);
+    }
+
+    public static ArrayList<Integer> get_user_conversations(int id) {
+        ArrayList<Integer> conversations = new ArrayList<Integer>();
+
+        String querry = String.format("Select conversation_id from %s where user_id='%s'",
+                DatabseInformation.USER_CONVERSATION_TABLE.value(), id);
+
+        ResultSet result;
+        try {
+
+            Connection connection = DriverManager.getConnection(DatabseInformation.URL.value(),
+                    DatabseInformation.USER.value(), DatabseInformation.PASSWORD.value());
+
+            Statement stat = connection.createStatement();
+            String request = String.format(querry);
+
+            result = stat.executeQuery(request);
+
+            while (result.next()) {
+                conversations.add(result.getInt(1));
+            }
+
+            connection.close();
+        } catch (Exception e) {
+            System.out.println(e);
+
+        } finally {
+
+        }
+
+        return conversations;
+    }
+
+    private static Hashtable<String, String> process_result_to_full_data(ResultSet result) {
+        Hashtable<String, String> user_data = new Hashtable<String, String>();
+
         try {
             while (result.next()) {
                 user_data.put("ID", result.getString(1));
@@ -33,27 +87,8 @@ public class UserDataAccesor implements DataAccesorInterface {
         return user_data;
     }
 
-    public static Hashtable<String, String> get_data(String column_name, String column_value) {
-        Hashtable<String, String> usr_data = new Hashtable<String, String>();
-        return usr_data;
-    }
-
-    public static Hashtable<String, String> get_data(String column_name, int column_value) {
-        Hashtable<String, String> usr_data = new Hashtable<String, String>();
-        return usr_data;
-    }
-
-    public static Hashtable<String, String> get_data_with_email(String email) {
-        Hashtable<String, String> usr_data = new Hashtable<String, String>();
-        return usr_data;
-    }
-
-    public static ArrayList<Integer> get_user_conversations(int id) {
-        ArrayList<Integer> conversations = new ArrayList<Integer>();
-        return conversations;
-    }
-
-    private static ResultSet get_querryResult(String querry) {
+    private static Hashtable<String, String> get_querry_result(String querry) {
+        Hashtable<String, String> user_data = new Hashtable<String, String>();
 
         ResultSet result = null;
 
@@ -66,6 +101,7 @@ public class UserDataAccesor implements DataAccesorInterface {
             String request = String.format(querry);
 
             result = stat.executeQuery(request);
+            user_data = process_result_to_full_data(result);
 
             connection.close();
         } catch (Exception e) {
@@ -74,6 +110,6 @@ public class UserDataAccesor implements DataAccesorInterface {
         } finally {
 
         }
-        return result;
+        return user_data;
     }
 }
