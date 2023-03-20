5a18cd06dfc11d56e5eb7554ec675a53592c5a46
mchomans <01168876@pw.edu.pl>
2023-03-20 15:11:07 +0100

    add table names

--- PAP_app_server/src/DatabaseInteractors/DatabseInformation.java
+++ PAP_app_server/src/DatabaseInteractors/DatabseInformation.java
@@ -1,7 +1,9 @@
 package DatabaseInteractors;
 
 public enum DatabseInformation {
-    PASSWORD(""), URL("jdbc:mysql://localhost:3306/PAP_app"), USER("root");
+    PASSWORD(""), URL("jdbc:mysql://localhost:3306/PAP_app"), USER("root"), USER_TABLE("users"),
+    MESSAGES_TABLE("messages"), CONVERSATION_TABLE("conversations"), FILES_TABLE("files"),
+    USER_CONVERSATION_TABLE("user_conversation_relation");
 
     private String _value;
 
