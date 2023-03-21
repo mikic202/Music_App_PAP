691abbc9d0d29ff7181f8a95a39aee738e9c04fa
mchomans <01168876@pw.edu.pl>
2023-03-21 19:50:18 +0100

    fix colum names

--- PAP_app_server/src/DatabaseInteractors/MessagesDatabaseInformation.java
+++ PAP_app_server/src/DatabaseInteractors/MessagesDatabaseInformation.java
@@ -2,8 +2,8 @@ package DatabaseInteractors;
 
 public enum MessagesDatabaseInformation {
     MESSAGES_TABLE("messages"),
-    MESSAGE_COLUMN("user_conversation_relation"), ID_COLUMN("message_id"), SENDER_COLUMN("username"),
-    CONVERSATION_COLUMN("email"), DATE_COLUMN("password");
+    MESSAGE_COLUMN("text"), ID_COLUMN("message_id"), SENDER_COLUMN("sendr_id"),
+    CONVERSATION_COLUMN("conversation_id"), DATE_COLUMN("creation_date");
 
     private String _value;
 
