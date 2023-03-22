39b72565514aa0f79a2f194b9620783e784ae65e
mchomans <01168876@pw.edu.pl>
2023-03-22 20:32:12 +0100

    add database information

--- PAP_app_server/src/DatabaseInteractors/FileDatabsaeInformation.java
+++ PAP_app_server/src/DatabaseInteractors/FileDatabsaeInformation.java
@@ -1,5 +1,17 @@
 package DatabaseInteractors;
 
-public class FileDatabsaeInformation {
+public enum FileDatabsaeInformation {
+    FILE_TABLE("files"),
+    ID_COLUMN("file_id"), NAME_COLUMN("file_name"),
+    USER_COLUMN("user_id"), FILEPATH_COLUMN("file_path");
 
+    private String _value;
+
+    FileDatabsaeInformation(String value) {
+        _value = value;
+    }
+
+    public String value() {
+        return _value;
+    }
 }
