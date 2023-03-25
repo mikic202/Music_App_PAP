06c7039366e5824488a17695591fd083528863ae
mchomans <01168876@pw.edu.pl>
2023-03-25 19:45:23 +0100

    add conversation message request procesing

--- PAP_app_server/src/Chat/Chat.java
+++ PAP_app_server/src/Chat/Chat.java
@@ -3,6 +3,9 @@ package Chat;
 import java.util.Hashtable;
 import java.util.ArrayList;
 
+import DatabaseInteractors.ConversationDataAccesor;
+import DatabaseInteractors.MessageDataAccesor;
+
 public class Chat {
     public ArrayList<Hashtable<String, String>> proces_requests(RequestTypes req_type, String request) {
         return generate_response(req_type, request);
@@ -10,6 +13,10 @@ public class Chat {
 
     private ArrayList<Hashtable<String, String>> _get_messeges_in_conversation(String request) {
         ArrayList<Hashtable<String, String>> response = new ArrayList<Hashtable<String, String>>();
+        ArrayList<Integer> messages = ConversationDataAccesor.get_mesages_in_conversation(Integer.parseInt(request));
+        for (Integer message_id : messages) {
+            response.add(MessageDataAccesor.get_data(message_id));
+        }
         return response;
     }
 
