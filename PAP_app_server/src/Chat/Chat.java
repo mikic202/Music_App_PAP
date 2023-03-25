a4902a61c6860655493502a0f16a735b5609c648
mchomans <01168876@pw.edu.pl>
2023-03-25 09:15:27 +0100

    add Chat class file

---
+++ PAP_app_server/src/Chat/Chat.java
@@ -0,0 +1,16 @@
+package Chat;
+
+public class Chat {
+    public String proces_requests(String request) {
+        return generate_response(request);
+    }
+
+    public void send_user_information() {
+    }
+
+    private String generate_response(String request) {
+        String response = new String();
+        return response;
+    }
+
+}
