93dfc49c1c8481969c0c029a60378bdcd0a9fc2d
mchomans <01168876@pw.edu.pl>
2023-03-25 19:14:25 +0100

    change function parameters and return types

--- PAP_app_server/src/Chat/Chat.java
+++ PAP_app_server/src/Chat/Chat.java
@@ -1,15 +1,18 @@
 package Chat;
 
+import java.util.Hashtable;
+import java.util.ArrayList;
+
 public class Chat {
-    public String proces_requests(String request) {
+    public ArrayList<Hashtable<String, String>> proces_requests(Hashtable<RequestTypes, String> request) {
         return generate_response(request);
     }
 
     public void send_user_information() {
     }
 
-    private String generate_response(String request) {
-        String response = new String();
+    private ArrayList<Hashtable<String, String>> generate_response(Hashtable<RequestTypes, String> request) {
+        ArrayList<Hashtable<String, String>> response = new ArrayList<Hashtable<String, String>>();
         return response;
     }
 
