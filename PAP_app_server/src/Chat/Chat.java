ffe5e5eacaf3b2fd55be0078e28b0ac561b2d673
mchomans <01168876@pw.edu.pl>
2023-03-25 19:21:49 +0100

    add case statement for messages in conversation

--- PAP_app_server/src/Chat/Chat.java
+++ PAP_app_server/src/Chat/Chat.java
@@ -4,15 +4,22 @@ import java.util.Hashtable;
 import java.util.ArrayList;
 
 public class Chat {
-    public ArrayList<Hashtable<String, String>> proces_requests(Hashtable<RequestTypes, String> request) {
-        return generate_response(request);
+    public ArrayList<Hashtable<String, String>> proces_requests(RequestTypes req_type, String request) {
+        return generate_response(req_type, request);
     }
 
-    public void send_user_information() {
-    }
-
-    private ArrayList<Hashtable<String, String>> generate_response(Hashtable<RequestTypes, String> request) {
+    private ArrayList<Hashtable<String, String>> _get_messeges_in_conversation(String request) {
         ArrayList<Hashtable<String, String>> response = new ArrayList<Hashtable<String, String>>();
+        return response;
+    }
+
+    private ArrayList<Hashtable<String, String>> generate_response(RequestTypes req_type, String request) {
+        ArrayList<Hashtable<String, String>> response = new ArrayList<Hashtable<String, String>>();
+        switch (req_type) {
+            case GET_MESSAGES:
+                response = _get_messeges_in_conversation(request);
+                break;
+        }
         return response;
     }
 
