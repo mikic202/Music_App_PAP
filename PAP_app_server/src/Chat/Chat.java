6c0ae86fad23006434e861399c8d43d47cef3e7d
mchomans <01168876@pw.edu.pl>
2023-03-25 20:05:45 +0100

    add conversation list request procesing

--- PAP_app_server/src/Chat/Chat.java
+++ PAP_app_server/src/Chat/Chat.java
@@ -5,10 +5,11 @@ import java.util.ArrayList;
 
 import DatabaseInteractors.ConversationDataAccesor;
 import DatabaseInteractors.MessageDataAccesor;
+import DatabaseInteractors.UserDataAccesor;
 
 public class Chat {
     public ArrayList<Hashtable<String, String>> proces_requests(RequestTypes req_type, String request) {
-        return generate_response(req_type, request);
+        return _generate_response(req_type, request);
     }
 
     private ArrayList<Hashtable<String, String>> _get_messeges_in_conversation(String request) {
@@ -20,11 +21,23 @@ public class Chat {
         return response;
     }
 
-    private ArrayList<Hashtable<String, String>> generate_response(RequestTypes req_type, String request) {
+    private ArrayList<Hashtable<String, String>> _get_users_conversations(String request) {
+        ArrayList<Hashtable<String, String>> response = new ArrayList<Hashtable<String, String>>();
+        ArrayList<Integer> conversations = UserDataAccesor.get_user_conversations(Integer.parseInt(request));
+        for (Integer conversation_id : conversations) {
+            response.add(ConversationDataAccesor.get_data(conversation_id));
+        }
+        return response;
+    }
+
+    private ArrayList<Hashtable<String, String>> _generate_response(RequestTypes req_type, String request) {
         ArrayList<Hashtable<String, String>> response = new ArrayList<Hashtable<String, String>>();
         switch (req_type) {
             case GET_MESSAGES:
                 response = _get_messeges_in_conversation(request);
+                break;
+            case GET_USERS_CONVERSATIONS:
+                response = _get_users_conversations(request);
                 break;
         }
         return response;
