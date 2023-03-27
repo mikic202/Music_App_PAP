ea612fbd47a658317a41c6c1a54943a7a7592d2a
mchomans <01168876@pw.edu.pl>
2023-03-27 14:32:49 +0200

    add returning information about added item

--- PAP_app_server/src/DatabaseInteractors/ConversationDatabsaeInformation.java
+++ PAP_app_server/src/DatabaseInteractors/ConversationDatabsaeInformation.java
@@ -2,7 +2,8 @@ package DatabaseInteractors;
 
 public enum ConversationDatabsaeInformation {
     CONVERSATION_TABLE("conversation"),
-    NAME_COLUMN("name"), ID_COLUMN("conversation_id"), NUMBER_OF_USERS_COLUMN("number_of_users");
+    NAME_COLUMN("name"), ID_COLUMN("conversation_id"), NUMBER_OF_USERS_COLUMN("number_of_users"),
+    USER_COLUMN("user_id");
 
     private String _value;
 
