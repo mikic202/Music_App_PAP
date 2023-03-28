af00957e8b5641b958205fd07bc53dd9320d7abe
mchomans <01168876@pw.edu.pl>
2023-03-28 19:25:56 +0200

    add

--- PAP_app_server/src/ClientServerConectivity/ChatConnector.java
+++ PAP_app_server/src/ClientServerConectivity/ChatConnector.java
@@ -1,5 +1,13 @@
 package ClientServerConectivity;
 
+import java.io.BufferedReader;
+import java.net.ServerSocket;
+
+import Chat.Chat;
+
 public class ChatConnector {
+    private ServerSocket chat_socket;
+    private Chat chat_processor;
+    private BufferedReader input;
 
 }
