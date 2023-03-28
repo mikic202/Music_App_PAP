c1eeda2913e4fdf957c4ad9f62af7f30e834cd08
mchomans <01168876@pw.edu.pl>
2023-03-28 20:48:47 +0200

    add creating add_conversation_request creator

--- PAP_app_user/src/Chat/RequestCreator.java
+++ PAP_app_user/src/Chat/RequestCreator.java
@@ -1,8 +1,19 @@
 package Chat;
 
+import java.util.ArrayList;
+
 public class RequestCreator {
     public static String create_send_msg_request(int conversation_id, int sender_id, String text) {
         return String.format("%s;%d;%d;%s", RequestTypes.SEND_MESSAGE.value(), sender_id, conversation_id,
                 text);
+    }
+
+    public static String create_add_conversation_request(String name, ArrayList<Integer> users) {
+        String request = RequestTypes.CREATE_CONVERSATION.value();
+        request += ";" + name;
+        for (int user_id : users) {
+            request += ";" + Integer.toString(user_id);
+        }
+        return request;
     }
 }
