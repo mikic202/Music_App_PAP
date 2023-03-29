02a4dd81a03348fd35ee145e68260cd38381cb11
mchomans <01168876@pw.edu.pl>
2023-03-29 21:03:37 +0200

    add more methods for chta connectivity

--- PAP_app_user/src/Chat/ChatSender.java
+++ PAP_app_user/src/Chat/ChatSender.java
@@ -1,6 +1,7 @@
 package Chat;
 
 import java.net.Socket;
+import java.util.ArrayList;
 import java.util.Hashtable;
 
 public class ChatSender {
@@ -15,6 +16,30 @@ public class ChatSender {
     public Hashtable<String, String> send_message(int conversation_id, int sender_id, String text) {
         Hashtable<String, String> added_msg_data = new Hashtable<>();
         String procesed_request = RequestCreator.create_send_msg_request(conversation_id, sender_id, text);
+        return added_msg_data;
+    }
+
+    public Hashtable<String, String> add_conversation(String name, ArrayList<Integer> users_ids) {
+        Hashtable<String, String> added_msg_data = new Hashtable<>();
+        String procesed_request = RequestCreator.create_add_conversation_request(name, users_ids);
+        return added_msg_data;
+    }
+
+    public Hashtable<String, String> get_messages_in_conversation(int conversation_id) {
+        Hashtable<String, String> added_msg_data = new Hashtable<>();
+        String procesed_request = RequestCreator.create_get_messages_request(conversation_id);
+        return added_msg_data;
+    }
+
+    public Hashtable<String, String> get_users_conversations(int user_id) {
+        Hashtable<String, String> added_msg_data = new Hashtable<>();
+        String procesed_request = RequestCreator.create_get_conversations_request(user_id);
+        return added_msg_data;
+    }
+
+    public Hashtable<String, String> add_users_to_conversation(int conversation_id, ArrayList<Integer> users_ids) {
+        Hashtable<String, String> added_msg_data = new Hashtable<>();
+        String procesed_request = RequestCreator.create_add_user_to_conversation_request(conversation_id, users_ids);
         return added_msg_data;
     }
 
