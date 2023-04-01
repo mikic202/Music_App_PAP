949c2933448ee1caba5b13b9bf3dcb6d4ff67b87
mchomans <01168876@pw.edu.pl>
2023-04-01 18:49:08 +0200

    add send request method

--- PAP_app_user/src/ServerConnector/ServerConnector.java
+++ PAP_app_user/src/ServerConnector/ServerConnector.java
@@ -1,5 +1,41 @@
 package ServerConnector;
 
-public class ServerConnector implements Runnable {
+import org.json.JSONObject;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+
+public class ServerConnector {
+    Socket socket;
+    BufferedReader input;
+    OutputStreamWriter output;
+
+    public ServerConnector() {
+        try {
+            socket = new Socket("localhost", 8000);
+            input = new BufferedReader(
+                    new InputStreamReader(socket.getInputStream()));
+            output = new OutputStreamWriter(
+                    socket.getOutputStream(), StandardCharsets.UTF_8);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+    }
+
+    public JSONObject send_request(JSONObject request) {
+        String response = "";
+        try {
+            output.write(request.toString());
+            while (!input.ready()) {
+            }
+            response = input.readLine();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        return new JSONObject(response);
+    }
 }
