0afa21f466b366749454bc95984741c43dc38b68
mchomans <01168876@pw.edu.pl>
2023-04-04 20:34:33 +0200

    add get users in conversation request type

--- PAP_app_user/src/Chat/RequestTypes.java
+++ PAP_app_user/src/Chat/RequestTypes.java
@@ -3,7 +3,7 @@ package Chat;
 public enum RequestTypes {
     GET_MESSAGES("messages"), GET_USERS_CONVERSATIONS("user conversations"), SEND_MESSAGE("send message"),
     CREATE_CONVERSATION("create_conversation"), ADD_USER_TO_CONVERSATION("add user to cconversation"),
-    USER_INFO("user information");
+    USER_INFO("user information"), GET_USERS_IN_CONVERSATION("get users in conversation");
     ;
 
     private String _value;
