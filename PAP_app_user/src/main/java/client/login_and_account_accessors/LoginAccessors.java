1f350ab939816703712e86c7b320ca43085422cf
mchomans <01168876@pw.edu.pl>
2023-04-05 20:46:17 +0200

    move classes to different folder

--- PAP_app_user/src/main/java/client/login_and_account_accessors/LoginAccessors.java
+++ PAP_app_user/src/main/java/client/login_and_account_accessors/LoginAccessors.java
@@ -1,4 +1,5 @@
-package client.GUI;
+package client.login_and_account_accessors;
+
 import org.json.JSONObject;
 
 import client.ServerConnector.ServerConnector;
@@ -15,17 +16,19 @@ public class LoginAccessors {
     }
 
     public JSONObject send_user_register_data(String email, String nickname, char[] password, char[] confirm_password) {
-        JSONObject procesed_request = LoginRequestCreator.create_send_registration_request(email, nickname, password, confirm_password);
+        JSONObject procesed_request = LoginRequestCreator.create_send_registration_request(email, nickname, password,
+                confirm_password);
         JSONObject response = server_connector.send_request(procesed_request);
         return response;
     }
 
-    public JSONObject send_user_change_password_data(String nickname, char[] old_password, char[] confirm_old_password, char[] new_password, char[] confirm_new_password) {
-        JSONObject procesed_request = LoginRequestCreator.create_change_password_request(nickname, old_password, confirm_old_password, new_password, confirm_new_password);
+    public JSONObject send_user_change_password_data(String nickname, char[] old_password, char[] confirm_old_password,
+            char[] new_password, char[] confirm_new_password) {
+        JSONObject procesed_request = LoginRequestCreator.create_change_password_request(nickname, old_password,
+                confirm_old_password, new_password, confirm_new_password);
         JSONObject response = server_connector.send_request(procesed_request);
         return response;
     }
-
 
     private ServerConnector server_connector;
 
---
+++ PAP_app_user/src/main/java/client/login_and_account_accessors/LoginRequestCreator.java
@@ -0,0 +1,28 @@
+package client.login_and_account_accessors;
+
+import org.json.JSONObject;
+
+public class LoginRequestCreator {
+        public static JSONObject create_send_login_request(String email, char[] password) {
+                return new JSONObject(
+                                String.format("{\"type\":\"%s\", \"value\":{\"email\":%s, \"password\":%s}}",
+                                                LoginRequestTypes.SEND_LOGIN.value(), email, password.toString()));
+        }
+
+        public static JSONObject create_send_registration_request(String email, String nickname, char[] password,
+                        char[] confirm_password) {
+                return new JSONObject(
+                                String.format("{\"type\":\"%s\", \"value\":{\"email\":%s, \"nickname\":%s, \"password\":%s, \"confirm_password\":%s}}",
+                                                LoginRequestTypes.SEND_REGISTER.value(), email, nickname,
+                                                new String(password), new String(confirm_password)));
+        }
+
+        public static JSONObject create_change_password_request(String nickname, char[] old_password,
+                        char[] confirm_old_password, char[] new_password, char[] confirm_new_password) {
+                return new JSONObject(
+                                String.format("{\"type\":\"%s\", \"value\":{\"nickname\":%s, \"old_password\":%s, \"confirm_old_password\":%s, \"new_password\":%s, \"confirm_new_password\":%s}}",
+                                                LoginRequestTypes.SEND_CHANGE_PASSWORD.value(), nickname,
+                                                new String(old_password), new String(confirm_old_password),
+                                                new String(new_password), new String(confirm_new_password)));
+        }
+}
