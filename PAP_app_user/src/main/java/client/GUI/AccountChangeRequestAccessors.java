f496885bd237d3eda694e932b3821994a2578bab
adamczubak <01168880@pw.edu.pl>
2023-04-05 09:55:15 +0200

    added change account settings request

--- PAP_app_user/src/main/java/client/GUI/AccountChangeRequestAccessors.java
+++ PAP_app_user/src/main/java/client/GUI/AccountChangeRequestAccessors.java
@@ -1,5 +1,6 @@
 package client.GUI;
 import org.json.JSONObject;
+import java.awt.image.BufferedImage;
 
 import client.ServerConnector.ServerConnector;
 
@@ -8,14 +9,14 @@ public class AccountChangeRequestAccessors {
         this.server_connector = server_connector;
     }
 
-    public JSONObject send_user_login_data(String email, char[] password) {
-        JSONObject procesed_request = LoginRequestCreator.create_send_login_request(email, password);
+    public JSONObject send_account_data(String email, String nickname, char[] old_password, char[] new_password, char[] confirm_new_password) {
+        JSONObject procesed_request = AccountChangeRequestCreator.create_send_account_request(email, nickname, old_password, new_password, confirm_new_password);
         JSONObject response = server_connector.send_request(procesed_request);
         return response;
     }
 
-    public JSONObject send_user_register_data(String email, String nickname, char[] password, char[] confirm_password) {
-        JSONObject procesed_request = LoginRequestCreator.create_send_registration_request(email, nickname, password, confirm_password);
+    public JSONObject send_user_register_data(BufferedImage avatar) {
+        JSONObject procesed_request = AccountChangeRequestCreator.create_send_avatar_request(avatar);
         JSONObject response = server_connector.send_request(procesed_request);
         return response;
     }
