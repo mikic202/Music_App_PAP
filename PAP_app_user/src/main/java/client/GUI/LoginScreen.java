ea4db3fc9d796d543a4f0443ce964859d26099c1
wmlynik <01168900@pw.edu.pl>
2023-04-07 05:40:45 +0200

    Add socket and server_connector initialization

--- PAP_app_user/src/main/java/client/GUI/LoginScreen.java
+++ PAP_app_user/src/main/java/client/GUI/LoginScreen.java
@@ -24,13 +24,14 @@ public class LoginScreen extends javax.swing.JFrame {
         private ServerConnector server_connector;
         private LoginAccessors logging_accesor;
 
-        public LoginScreen() {
+        public LoginScreen(ServerConnector server_connector) {
                 // try {
                 // server_connector = new ServerConnector(new Socket("localhost", 8000));
                 // } catch (Exception e) {
                 // System.out.println(e);
                 // System.exit(-1);
                 // }
+        		this.server_connector = server_connector;
                 logging_accesor = new LoginAccessors(server_connector);
                 initComponents();
         }
@@ -306,50 +307,50 @@ public class LoginScreen extends javax.swing.JFrame {
         /**
          * @param args the command line arguments
          */
-        public static void main(String args[]) {
-                /* Set the Nimbus look and feel */
-                // <editor-fold defaultstate="collapsed" desc=" Look and feel setting code
-                // (optional) ">
-                /*
-                 * If Nimbus (introduced in Java SE 6) is not available, stay with the default
-                 * look and feel.
-                 * For details see
-                 * http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html
-                 */
-                try {
-                        for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager
-                                        .getInstalledLookAndFeels()) {
-                                if ("Nimbus".equals(info.getName())) {
-                                        javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                                        break;
-                                }
-                        }
-                } catch (ClassNotFoundException ex) {
-                        java.util.logging.Logger.getLogger(LoginScreen.class.getName()).log(
-                                        java.util.logging.Level.SEVERE, null,
-                                        ex);
-                } catch (InstantiationException ex) {
-                        java.util.logging.Logger.getLogger(LoginScreen.class.getName()).log(
-                                        java.util.logging.Level.SEVERE, null,
-                                        ex);
-                } catch (IllegalAccessException ex) {
-                        java.util.logging.Logger.getLogger(LoginScreen.class.getName()).log(
-                                        java.util.logging.Level.SEVERE, null,
-                                        ex);
-                } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-                        java.util.logging.Logger.getLogger(LoginScreen.class.getName()).log(
-                                        java.util.logging.Level.SEVERE, null,
-                                        ex);
-                }
-                // </editor-fold>
-
-                /* Create and display the form */
-                java.awt.EventQueue.invokeLater(new Runnable() {
-                        public void run() {
-                                new LoginScreen().setVisible(true);
-                        }
-                });
-        }
+//        public static void main(String args[]) {
+//                /* Set the Nimbus look and feel */
+//                // <editor-fold defaultstate="collapsed" desc=" Look and feel setting code
+//                // (optional) ">
+//                /*
+//                 * If Nimbus (introduced in Java SE 6) is not available, stay with the default
+//                 * look and feel.
+//                 * For details see
+//                 * http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html
+//                 */
+//                try {
+//                        for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager
+//                                        .getInstalledLookAndFeels()) {
+//                                if ("Nimbus".equals(info.getName())) {
+//                                        javax.swing.UIManager.setLookAndFeel(info.getClassName());
+//                                        break;
+//                                }
+//                        }
+//                } catch (ClassNotFoundException ex) {
+//                        java.util.logging.Logger.getLogger(LoginScreen.class.getName()).log(
+//                                        java.util.logging.Level.SEVERE, null,
+//                                        ex);
+//                } catch (InstantiationException ex) {
+//                        java.util.logging.Logger.getLogger(LoginScreen.class.getName()).log(
+//                                        java.util.logging.Level.SEVERE, null,
+//                                        ex);
+//                } catch (IllegalAccessException ex) {
+//                        java.util.logging.Logger.getLogger(LoginScreen.class.getName()).log(
+//                                        java.util.logging.Level.SEVERE, null,
+//                                        ex);
+//                } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+//                        java.util.logging.Logger.getLogger(LoginScreen.class.getName()).log(
+//                                        java.util.logging.Level.SEVERE, null,
+//                                        ex);
+//                }
+//                // </editor-fold>
+//
+//                /* Create and display the form */
+//                java.awt.EventQueue.invokeLater(new Runnable() {
+//                        public void run() {
+//                                new LoginScreen().setVisible(true);
+//                        }
+//                });
+//        }
 
         // Variables declaration - do not modify//GEN-BEGIN:variables
         private javax.swing.JButton jButton1;
