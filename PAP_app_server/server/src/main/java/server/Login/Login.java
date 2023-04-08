74d585d5f548a2a3d2ccd4ab03ee8549a0cefb3f
wmlynik <01168900@pw.edu.pl>
2023-04-08 17:20:08 +0200

    Fix sending login-related requests, add registration and password change handling

--- PAP_app_server/server/src/main/java/server/Login/Login.java
+++ PAP_app_server/server/src/main/java/server/Login/Login.java
@@ -7,13 +7,15 @@ import java.util.Set;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import server.DatabaseInteractors.UserDataAccesor;
+import server.DatabaseInteractors.UserDataSetter;
+import server.DatabaseInteractors.UserDatabaseInformation;
 
 public class Login {
     public static JSONObject proces_requests(LoginRequestTypes req_type, JSONObject request) {
         return _generate_response(req_type, request);
     }
 
-    private static JSONObject _get_existance(JSONObject request) {
+    private static JSONObject _login(JSONObject request) {
         String wanted_email = request.getString("email");
         JSONObject result = new JSONObject();
         result.put("type", LoginRequestTypes.SEND_LOGIN.value());
@@ -32,43 +34,80 @@ public class Login {
                 JSONObject true_result = new JSONObject();
                 true_result.put("outcome", true);
                 true_result.put("username", user_info.get("username"));
-                true_result.put("user_id", user_info.get("user_id"));
+                true_result.put("user_id", user_info.get("ID"));
                 result.put("value", true_result);
             }
             else
             {
-                JSONObject true_result = new JSONObject();
-                true_result.put("outcome", true);
-                result.put("value", true_result);
+                JSONObject false_result = new JSONObject();
+                false_result.put("outcome", false);
+                result.put("value", false_result);
             }
         }
         return result;
-    } 
-
-    private static JSONObject _convert_response_to_json(ArrayList<Hashtable<String, String>> response, LoginRequestTypes req_type) {
-        JSONObject json_response = new JSONObject();
-        json_response.put("type", req_type.value());
-        JSONArray json_response_value = new JSONArray();
-        for (Hashtable<String, String> element : response) {
-            Set<String> keys = element.keySet();
-            JSONObject json_element = new JSONObject();
-            for (String key : keys) {
-                json_element.put(key, element.get(key));
-            }
-            json_response_value.put(json_element);
+    }
+    
+    private static JSONObject _register(JSONObject request) {
+        String email = request.getString("email");
+        String nickname = request.getString("nickname");
+        String password = request.getString("password");
+        String confirm_password = request.getString("confirm_password");
+        JSONObject result = new JSONObject();
+        result.put("type", LoginRequestTypes.SEND_REGISTER.value());
+        if(!password.equals(confirm_password)) {
+        	result.put("outcome", false);
+        	return result;
         }
-        json_response.put("value", json_response_value);
-        return json_response;
+        Hashtable<String, String> user_info = UserDataAccesor.get_data_with_email(email);
+        if(!user_info.isEmpty()) {
+        	result.put("outcome", false);
+        	return result;
+        }
+        user_info = UserDataAccesor.get_data_with_name(nickname);
+        if(!user_info.isEmpty()) {
+        	result.put("outcome", false);
+        	return result;
+        }
+        var data = new Hashtable<String, String>();
+        data.put("username", nickname);
+        data.put("email", email);
+        data.put("password", password);
+        UserDataSetter.add_data(data);
+        result.put("outcome", true);
+        return result;
+    	
+    }
+    
+    private static JSONObject _change_password(JSONObject request) {
+        String email = request.getString("email");
+        String old_password = request.getString("old_password");
+        String confirm_old_password = request.getString("confirm_old_password");
+        String new_password = request.getString("new_password");
+        String confirm_new_password = request.getString("confirm_new_password");
+        JSONObject result = new JSONObject();
+    	if(!old_password.equals(confirm_old_password) || !new_password.equals(confirm_new_password)) {
+        	result.put("outcome", false);
+        	return result;
+    	}
+        Hashtable<String, String> user_info = UserDataAccesor.get_data_with_email(email);
+        if(user_info.isEmpty()) {
+        	result.put("outcome", false);
+        	return result;
+        }
+        user_info.put("password", confirm_new_password);
+        UserDataSetter.set_data(0, user_info);
+        result.put("outcome", true);
+        return result;
     }
 
     private static JSONObject _generate_response(LoginRequestTypes req_type, JSONObject request) {
         JSONObject response = new JSONObject();
         switch (req_type) {
             case SEND_LOGIN:
-                response = _get_existance(request);
+                response = _login(request);
                 break;
             case SEND_REGISTER:
-                
+                response = _register(request);
                 break;
             case SEND_CHANGE_PASSWORD:
                 
