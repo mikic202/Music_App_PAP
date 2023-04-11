df331e6001de7e82cdc167fdb7e1432ecdbfa9ef
wmlynik <01168900@pw.edu.pl>
2023-04-12 00:42:15 +0200

    Add file upload sopport on client side

---
+++ PAP_app_user/src/main/java/client/files/FileUploader.java
@@ -0,0 +1,42 @@
+package client.files;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.Socket;
+
+import client.ServerConnector.ServerConnector;
+
+public class FileUploader implements Runnable {
+	private FileInputStream input;
+	private String uuid;
+	private UploadAccessors accessors;
+	
+	public FileUploader(File file, String uuid, UploadAccessors accessors) throws FileNotFoundException {
+		input = new FileInputStream(file);
+		this.uuid = uuid;
+		this.accessors = accessors;
+	}
+
+	@Override
+	public void run() {
+		Socket socket = null;
+		try {
+			socket = new Socket("144.91.114.89", 8001);
+			input.transferTo(socket.getOutputStream());
+			accessors.finishUpload(uuid);
+		} catch (IOException e) {
+			e.printStackTrace();
+			return;
+		}
+		finally {
+			try {
+				socket.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+}
---
+++ PAP_app_user/src/main/java/client/files/UploadAccessors.java
@@ -0,0 +1,24 @@
+package client.files;
+
+import org.json.JSONObject;
+
+import client.ServerConnector.ServerConnector;
+
+public class UploadAccessors {
+	private ServerConnector serverConnector;
+	
+	public UploadAccessors(ServerConnector serverConnector) {
+		this.serverConnector = serverConnector;
+	}
+	public JSONObject startUpload(int user_id, String file_name) {
+        JSONObject procesed_request = UploadRequestCreator.create_start_upload_request(user_id, file_name);
+        JSONObject response = serverConnector.send_request(procesed_request);
+        return response;
+    }
+	
+	public JSONObject finishUpload( String uuid) {
+        JSONObject procesed_request = UploadRequestCreator.create_finish_upload_request(uuid);
+        JSONObject response = serverConnector.send_request(procesed_request);
+        return response;
+    }
+}
