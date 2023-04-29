72aad7239651f0d2dc19f7c3089b1bd1d622ec31
mchomans <01168876@pw.edu.pl>
2023-04-29 10:31:55 +0200

    change naming convention to camelCase

--- PAP_app_user/src/main/java/client/Chat/RequestCreator.java
+++ PAP_app_user/src/main/java/client/Chat/RequestCreator.java
@@ -5,32 +5,32 @@ import java.util.ArrayList;
 import org.json.JSONObject;
 
 public class RequestCreator {
-        public static JSONObject create_send_msg_request(int conversation_id, int sender_id, String text) {
+        public static JSONObject createSendMsgRequest(int conversation_id, int sender_id, String text) {
                 return new JSONObject(
                                 String.format("{\"type\":\"%s\", \"value\":{\"sender_id\":%d, \"conversation_id\":%d, \"text\":%s}}",
                                                 RequestTypes.SEND_MESSAGE.value(), sender_id, conversation_id, text));
         }
 
-        public static JSONObject create_add_conversation_request(String name, ArrayList<Integer> users) {
+        public static JSONObject createAddRonversationRequest(String name, ArrayList<Integer> users) {
                 return new JSONObject(
                                 String.format("{\"type\":\"%s\", \"value\":{\"name\":%s, \"users\":%s}}",
                                                 RequestTypes.CREATE_CONVERSATION.value(), name, users));
         }
 
-        public static JSONObject create_get_messages_request(int conversation_id) {
+        public static JSONObject createGetMessagesRequest(int conversation_id) {
                 return new JSONObject(
                                 String.format("{\"type\":\"%s\", \"value\":{\"conversation_id\":%s}}",
                                                 RequestTypes.GET_MESSAGES.value(), conversation_id));
 
         }
 
-        public static JSONObject create_get_conversations_request(int user_id) {
+        public static JSONObject createGetConversationsRequest(int user_id) {
                 return new JSONObject(
                                 String.format("{\"type\":\"%s\", \"value\":{\"user_id\":%s}}",
                                                 RequestTypes.GET_USERS_CONVERSATIONS.value(), user_id));
         }
 
-        public static JSONObject create_add_user_to_conversation_request(int conversation_id,
+        public static JSONObject createAddUserToConversationRequest(int conversation_id,
                         ArrayList<Integer> users_ids) {
                 return new JSONObject(
                                 String.format("{\"type\":\"%s\", \"value\":{\"conversation_id\":%s, \"users\":%s}}",
@@ -38,25 +38,25 @@ public class RequestCreator {
                                                 users_ids));
         }
 
-        public static JSONObject create_get_user_information_request(int user_id) {
+        public static JSONObject createGetUserInformationRequest(int user_id) {
                 return new JSONObject(
                                 String.format("{\"type\":\"%s\", \"value\":{\"user_id\":%s, \"type\": user_id}}",
                                                 RequestTypes.USER_INFO.value(), user_id));
         }
 
-        public static JSONObject create_get_user_information_request(String username) {
+        public static JSONObject createGetUserInformationRequest(String username) {
                 return new JSONObject(
                                 String.format("{\"type\":\"%s\", \"value\":{\"username\":%s, \"type\": username}}",
                                                 RequestTypes.USER_INFO.value(), username));
         }
 
-        public static JSONObject get_users_in_conversation(int conversation_id) {
+        public static JSONObject getUsersInConversation(int conversation_id) {
                 return new JSONObject(
                                 String.format("{\"type\":\"%s\", \"value\":{\"conversation_id\":%s}}",
                                                 RequestTypes.GET_USERS_IN_CONVERSATION.value(), conversation_id));
         }
 
-        public static JSONObject create_get_new_messages_in_conversation(int conversation, Integer latest_message) {
+        public static JSONObject createGetNewMessagesInConversation(int conversation, Integer latest_message) {
                 return new JSONObject(
                                 String.format("{\"type\":\"%s\", \"value\":{\"conversation_id\":%s, \"latest_message\":%s}}",
                                                 RequestTypes.GET_LATEST_MESSAGE.value(), conversation, latest_message));
