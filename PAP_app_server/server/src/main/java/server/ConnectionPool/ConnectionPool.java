3e6b4d85e7421a7870ff23d292e0e4a87b4e6516
mchomans <01168876@pw.edu.pl>
2023-04-30 10:24:48 +0200

    add basic connection pool

---
+++ PAP_app_server/server/src/main/java/server/ConnectionPool/ConnectionPool.java
@@ -0,0 +1,36 @@
+package server.ConnectionPool;
+
+import server.DatabaseInteractors.DatabseInformation;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class ConnectionPool {
+    private static ArrayList<Connection> availableConnections = new ArrayList<>();
+    private static ArrayList<Connection> usedConnections = new ArrayList<>();
+    final private int MAX_POOL_SIZE = 5;
+    {
+        System.out.println("Activating connections");
+        try {
+            for (int i = 0; i < MAX_POOL_SIZE; i++) {
+                availableConnections.add(DriverManager.getConnection(DatabseInformation.URL.value(),
+                        DatabseInformation.USER.value(), DatabseInformation.PASSWORD.value()));
+            }
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+    };
+
+    public ConnectionPool() {
+        System.out.println("Connection Pool Active");
+    }
+
+    public static Connection getConnection() {
+        Connection connection = availableConnections
+                .remove(availableConnections.size() - 1);
+        usedConnections.add(connection);
+        return connection;
+    }
+}
