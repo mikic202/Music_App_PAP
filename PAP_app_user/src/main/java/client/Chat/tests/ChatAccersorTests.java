6d12e39effc945c2c5587ed7cdce43d09a5202e5
mchomans <01168876@pw.edu.pl>
2023-06-04 20:24:07 +0200

    prepare tests

--- PAP_app_user/src/main/java/client/Chat/tests/ChatAccersorTests.java
+++ PAP_app_user/src/main/java/client/Chat/tests/ChatAccersorTests.java
@@ -1,26 +1,52 @@
 package client.Chat.tests;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.junit.Assert;
+import org.junit.Before;
 import org.mockito.MockedStatic;
 import org.mockito.Mockito;
 
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import client.Chat.ChatAccesors;
 import client.ServerConnector.ServerConnector;
 
 public class ChatAccersorTests {
 
+    private ServerConnector serverConnector;
+    private ChatAccesors chatAccesor;
+
+    // @Before
+    // public void setUp() {
+    // // mathApplication = new MathApplication();
+    // // calcService = mock(CalculatorService.class);
+    // // mathApplication.setCalculatorService(calcService);
+    // }
+    @Before
+    public void setUp() {
+        serverConnector = mock(ServerConnector.class);
+        chatAccesor = new ChatAccesors(serverConnector);
+    }
+
     @Test
     public void testUserInfoRequest() {
+        //
+        when
+        // try (MockedStatic<ServerConnector> dummyAccesor =
+        // Mockito.mockStatic(ServerConnector.class)) {
+        // Hashtable<String, String> userData = new Hashtable<String, String>();
 
-        try (MockedStatic<ServerConnector> dummyAccesor = Mockito.mockStatic(ServerConnector.class)) {
-            Hashtable<String, String> userData = new Hashtable<String, String>();
-
-            dummyAccesor.when(() -> UserDataAccesor.getData(1)).thenReturn(userData);
-            var expectedResponse = _convertResponseToJson(userData, RequestTypes.USER_INFO);
-            expectedResponse.getJSONObject("value").remove("password");
-            Assert.assertEquals(
-                    expectedResponse.toString(),
-                    Chat.procesRequests(RequestTypes.USER_INFO, request).toString());
-        }
+        //
+        // dummyAccesor.when(() -> ServerConnector.send).thenReturn(userData);
+        // var expectedResponse = _convertResponseToJson(userData,
+        // RequestTypes.USER_INFO);
+        // Response.getJSONObject("value").remove("password");
+        // ssertEquals(
+        // expectedResponse.toString(),
+        // Chat.procesRequests(RequestTypes.USER_INFO, request).toString());
+        // }
     }
 }
