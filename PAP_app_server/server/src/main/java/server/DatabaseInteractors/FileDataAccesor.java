ed6d69d8c468e12f9af95eff1b5b338eb7d17563
wmlynik <01168900@pw.edu.pl>
2023-06-05 14:41:01 +0200

    Add getting song list from server

--- PAP_app_server/server/src/main/java/server/DatabaseInteractors/FileDataAccesor.java
+++ PAP_app_server/server/src/main/java/server/DatabaseInteractors/FileDataAccesor.java
@@ -3,7 +3,9 @@ package server.DatabaseInteractors;
 import java.util.ArrayList;
 import java.util.Enumeration;
 import java.util.Hashtable;
+import java.util.ListIterator;
 
+import server.ClientHandlers.Client;
 import server.ConnectionPool.ConnectionPool;
 
 import java.sql.Connection;
@@ -13,16 +15,16 @@ public class FileDataAccesor implements DataAccesorInterface {
 
     static final String TABLENAME = FileDatabsaeInformation.FILE_TABLE.value();
 
-    public static Hashtable<String, String> getData(int id) {
+    public static ArrayList<Hashtable<String, String>> getData(int id) {
         return getData(FileDatabsaeInformation.ID_COLUMN.value(), id);
     }
 
-    public static Hashtable<String, String> getData(String column_name, String column_value) {
+    public static ArrayList<Hashtable<String, String>> getData(String column_name, String column_value) {
         String preparedStatements = String.format("Select * from %s where %s=?", TABLENAME, column_name);
         return getQuerryResult(preparedStatements, column_value);
     }
 
-    public static Hashtable<String, String> getData(String column_name, int column_value) {
+    public static ArrayList<Hashtable<String, String>> getData(String column_name, int column_value) {
         String preparedStatements = String.format("Select * from %s where %s=?", TABLENAME, column_name);
         return getQuerryResult(preparedStatements, column_value);
     }
@@ -31,15 +33,21 @@ public class FileDataAccesor implements DataAccesorInterface {
         String songIdStatement = String.format("Select * from %s where %s=?",
                 TABLENAME, FileDatabsaeInformation.USER_COLUMN.value());
 
-        Hashtable<String, String> songIdData = getQuerryResult(songIdStatement, userId);
+        ArrayList<Hashtable<String, String>> songIdData = getQuerryResult(songIdStatement, userId);
         Hashtable<String, Integer> files = new Hashtable<String, Integer>();
+        System.out.println(songIdData);
 
-        Enumeration<String> e = songIdData.keys();
-        while(e.hasMoreElements())
-        {
-            String key = e.nextElement();
-            String id = songIdData.get(key);
-            files.put(key, Integer.parseInt(id));
+        ListIterator<Hashtable<String, String>> it = songIdData.listIterator();
+        
+        while(it.hasNext()) {
+        	Hashtable<String, String> file = it.next();
+
+        	System.out.println(file.get("file_name"));
+        	System.out.println(file.get("is_image"));
+        	if(file.get("is_image").equals("1")) {
+        		continue;
+        	}
+        	files.put(file.get("file_name"), Integer.parseInt(file.get("ID")));
         }
 
         if(files.isEmpty())
@@ -77,25 +85,28 @@ public class FileDataAccesor implements DataAccesorInterface {
         return files;
     }
 
-    private static Hashtable<String, String> processResultToFullData(ResultSet result) {
-        Hashtable<String, String> user_data = new Hashtable<String, String>();
+    private static ArrayList<Hashtable<String, String>> processResultToFullData(ResultSet result) {
+        ArrayList<Hashtable<String, String>> ret = new ArrayList<Hashtable<String, String>>();
 
         try {
             while (result.next()) {
+                Hashtable<String, String> user_data = new Hashtable<String, String>();
                 user_data.put("ID", result.getString(1));
                 user_data.put("file_name", result.getString(2));
                 user_data.put("user", result.getString(3));
                 user_data.put("file_path", result.getString(4));
+                user_data.put("is_image", result.getString(5));
+                ret.add(user_data);
             }
         } catch (Exception e) {
             System.out.println(e);
         }
 
-        return user_data;
+        return ret;
     }
 
-    private static Hashtable<String, String> getQuerryResult(String preparedStatement, int value) {
-        Hashtable<String, String> fileData = new Hashtable<String, String>();
+    private static ArrayList<Hashtable<String, String>> getQuerryResult(String preparedStatement, int value) {
+    	ArrayList<Hashtable<String, String>> fileData = new ArrayList<Hashtable<String, String>>();
 
         ResultSet result = null;
         Connection connection = ConnectionPool.getConnection();
@@ -114,8 +125,8 @@ public class FileDataAccesor implements DataAccesorInterface {
         return fileData;
     }
 
-    private static Hashtable<String, String> getQuerryResult(String preparedStatement, String value) {
-        Hashtable<String, String> fileData = new Hashtable<String, String>();
+    private static ArrayList<Hashtable<String, String>> getQuerryResult(String preparedStatement, String value) {
+    	ArrayList<Hashtable<String, String>> fileData = new ArrayList<Hashtable<String, String>>();
 
         ResultSet result = null;
         Connection connection = ConnectionPool.getConnection();
